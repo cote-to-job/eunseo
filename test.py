f63c5abbe068db0645a2f4aaf68625f5e06b7b91
Lisa-Tera <eun01632@naver.com>
2025-03-07 21:39:54 +0900

    0307
    hash_2

--- test.py
+++
@@ -1,9 +0,0 @@
-# 목표: c에 없는 사람 return
-def solution(participant, completion): #p:참여자, c: 완주자
-    participant.sort()
-    completion.sort()
-    len(participant)
-    for p,c in zip(participant, completion):
-        if p != c: 
-            return p
-    return participant[-1]
