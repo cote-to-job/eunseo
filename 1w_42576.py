559ff982f62ec143c31649dd01e128dbe0cdd0e3
Lisa-Tera <56185149+Lisa-Tera@users.noreply.github.com>
2025-03-10 10:37:59 +0900

    Update 1w_42576.py

--- 1w_42576.py
+++ 1w_42576.py
@@ -2,7 +2,6 @@
 def solution(participant, completion): #p:참여자, c: 완주자
     participant.sort()
     completion.sort()
-    len(participant)
     for p,c in zip(participant, completion):
         if p != c: 
             return p
